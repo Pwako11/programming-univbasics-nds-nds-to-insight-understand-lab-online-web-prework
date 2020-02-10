a9332878b924655a73a32f9b5cf4798244ea5b37
Patrick A <akolo@hotmail.com>
2020-02-10 01:04:15 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,15 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  
+  row_index = 0 
+  while row_index < print_first_directors_movie_titles.length do 
+    puts \n row_index
+    row_index += 1
+  end 
+  
 end
